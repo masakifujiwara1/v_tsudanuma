db31f25db6901e9f7e8d8dce96dec88f4b1443ca
MasakiFujiwara <fmasaki2020@gmail.com>
2023-04-06 17:47:31 +0900

    update

--- experiments/experiment_outdoor.sh
+++ experiments/experiment_outdoor.sh
@@ -1,5 +1,5 @@
 for i in `seq 1`
 do
-  roslaunch v_tsudanuma nav_cloning_sim.launch script:=nav_cloning_with_direction_node.py mode:=selected_training world_name:=tsudanuma2goukan3.world map_file:=tsudanuma2goukan.yaml waypoints_file:=real_tsudanuma2-3_way.yaml dist_err:=0.8 initial_pose_x:=10.6 initial_pose_y:=-35.4 initial_pose_a:=1.57 use_waypoint_nav:=true robot_x:=10.724 robot_y:=-35.322 robot_Y:=1.6211
+  roslaunch v_tsudanuma nav_cloning_sim.launch script:=nav_cloning_with_direction_node.py mode:=selected_training world_name:=tsudanuma2goukan3_actor.world map_file:=tsudanuma2goukan waypoints_file:=tsudanuma2goukan_way.yaml dist_err:=0.8 initial_pose_x:=10.6 initial_pose_y:=-35.4 initial_pose_a:=1.57 use_waypoint_nav:=true robot_x:=10.724 robot_y:=-35.322 robot_Y:=1.6211
   sleep 10
 done
